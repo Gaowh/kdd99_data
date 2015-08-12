77d5a078f736751534191be3f6fd64185b61b323
Gaowh <411867865@qq.com>
2015-08-12 11:35:35 +0800

    ok

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 source ./scripts/get_fields.sh
-source ./scripts/ipv4_get_kdd99.sh
-source ./scripts/ipv6_get_kdd99.sh
+source ./scripts/ipv4_get_pre_data.sh
+source ./scripts/ipv6_get_pre_data.sh
 source ./scripts/func.sh
 
 while getopts :r: opt
@@ -25,16 +25,18 @@ fi
 mkdir ./ipv4
 mkdir ./ipv6
 
+#下面三个目录用来保存iPv4的三类数据
 mkdir ./ipv4/tcpstream
 mkdir ./ipv4/udpstream
 mkdir ./ipv4/icmpstream
 
+
+#下面三个目录用来保存ipv6的三类数据
 mkdir ./ipv6/tcpstream
 mkdir ./ipv6/udpstream
 mkdir ./ipv6/icmpstream
 
-#分ipv4和ipv6的数据
-
+#分离ipv4和ipv6的数据
 tshark -r $infile -R "ip" -w ./ipv4/ipv4dat
 tshark -r $infile -R "ipv6" -w ./ipv6/ipv6dat
 
@@ -42,8 +44,7 @@ ipv4dat=./ipv4/ipv4dat
 ipv6dat=./ipv6/ipv6dat
 
 
-#对ipv4数据包分流
-
+#对ipv4的三类数据包进行分离
 tshark -r $ipv4dat -R "tcp" -w ./ipv4/tcpdat
 tshark -r $ipv4dat -R "udp" -w ./ipv4/udpdat
 tshark -r $ipv4dat -R "icmp" -w ./ipv4/icmpdat
@@ -56,6 +57,8 @@ tcpstreampath=./ipv4/tcpstream
 udpstreampath=./ipv4/udpstream
 icmpstreampath=./ipv4/icmpstream
 
+
+#将tcp数据分流
 i=1
 while [ "$i" ];do
     cmd="tcp.stream eq $i"
@@ -69,14 +72,17 @@ while [ "$i" ];do
     i=`expr $i + 1`
 done
 
+#将udp数据以单独的一个包进行分流
 editcap -c 1 $udpdat $udpstreampath/udpstream
+
+#将icmp数据以单独的一个包进行分流
 editcap -c 1 $icmpdat $icmpstreampath/icmpstream
 
 rm -rf $tcpdat
 rm -rf $udpdat
 rm -rf $icmpdat
 
-#对ipv6数据包分流
+#对ipv6的三类数据包进行分流
 tshark -r $ipv6dat -R "tcp" -w ./ipv6/tcpdat
 tshark -r $ipv6dat -R "udp" -w ./ipv6/udpdat
 tshark -r $ipv6dat -R "icmpv6" -w ./ipv6/icmpdat
@@ -89,6 +95,8 @@ tcpstreampath6=./ipv6/tcpstream
 udpstreampath6=./ipv6/udpstream
 icmpstreampath6=./ipv6/icmpstream
 
+
+#将ipv6的tcp进行分流
 i=1
 while [ "$i" ];do
     cmd="tcp.stream eq $i"
@@ -102,6 +110,7 @@ while [ "$i" ];do
     i=`expr $i + 1`
 done
 
+#将ipv6的udp和icmp进行分流
 editcap -c 1 $udpdat $udpstreampath6/udpstream
 editcap -c 1 $icmpdat $icmpstreampath6/icmpstream
 
@@ -115,16 +124,29 @@ rm -rf $ipv6dat
 echo Split_stream Done!
 echo call fields func!
 
+
+#下面从每个流中提取出某些特征，该特征用于之后的kdd99特征的提取
 mkdir ./fields_ipv4
 mkdir ./fields_ipv6
 ipv4_get_fields $tcpstreampath $udpstreampath $icmpstreampath ./fields_ipv4
 ipv6_get_fields $tcpstreampath6 $udpstreampath6 $icmpstreampath6 ./fields_ipv6
 
+
+#从上面提取的特征中提取出一部分kdd99特征和一些辅助的特征
 ipv4_tcp_get_kdd99 ./fields_ipv4/tcpfields ./pre_kdd99.dat
 ipv4_udp_get_kdd99 ./fields_ipv4/udpfields ./pre_kdd99.dat
 ipv4_icmp_get_kdd99 ./fields_ipv4/icmpfields ./pre_kdd99.dat
-
 ipv6_tcp_get_kdd99 ./fields_ipv6/tcpfields ./pre_kdd99.dat
 ipv6_udp_get_kdd99 ./fields_ipv6/udpfields ./pre_kdd99.dat
 ipv6_icmp_get_kdd99 ./fields_ipv6/icmpfields ./pre_kdd99.dat
 
+#将流按照时间排序
+pre_sort ./pre_kdd99.dat
+
+get_kdd99_all ./pre_kdd99.dat ./kdd99_all.dat
+
+rm -rf ./fields_ipv4
+rm -rf ./fields_ipv6
+
+rm -rf ./ipv4
+rm -rf ./ipv6
